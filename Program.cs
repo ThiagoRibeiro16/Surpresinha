d6525c90ff85f8c3268c8fab416cc112d13bb0cf
Thiago Ribeiro <thiago.escola321@gmail.com>
2025-06-22 20:38:36 -0300

    finaliza projeto

---
+++ Program.cs
@@ -0,0 +1,103 @@
+﻿using System.Security.Cryptography;
+
+Console.Clear();
+
+Console.WriteLine("Bem-vindo ao Surpresinha!\n");
+
+const int QuantidadeNumerosPorSequencia = 6;
+const int ValorMaximoNumero = 99;
+
+bool continuar = true;
+
+while (continuar)
+{
+    Console.WriteLine("--- Geração de sequências de números Aleatórios ---\n");
+
+    int quantidadeSequencias;
+
+    while (true)
+    {
+        Console.Write("Digite o número de sequências desejadas: ");
+
+        string? entrada = Console.ReadLine()!;
+
+        bool entradaValida = int.TryParse(entrada, out quantidadeSequencias);
+
+        if (!entradaValida || quantidadeSequencias <= 0)
+        {
+            Console.WriteLine("Entrada inválida. Digite um número inteiro maior que 0.\n");
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    Console.WriteLine();
+
+    int[][] listaSequencias = new int[quantidadeSequencias][];
+
+    for (int indiceSequencia = 0; indiceSequencia < quantidadeSequencias; indiceSequencia++)
+    {
+        int[] sequenciaAtual = new int[QuantidadeNumerosPorSequencia];
+
+        int indiceNumero = 0;
+        while (indiceNumero < QuantidadeNumerosPorSequencia)
+        {
+            int numeroSorteado = RandomNumberGenerator.GetInt32(1, ValorMaximoNumero + 1);
+
+            bool repetido = false;
+            for (int i = 0; i < indiceNumero; i++)
+            {
+                if (sequenciaAtual[i] == numeroSorteado)
+                {
+                    repetido = true;
+                    break;
+                }
+            }
+
+            if (repetido)
+            {
+                continue;
+            }
+
+            sequenciaAtual[indiceNumero] = numeroSorteado;
+            indiceNumero++;
+        }
+
+        Array.Sort(sequenciaAtual);
+
+        listaSequencias[indiceSequencia] = sequenciaAtual;
+    }
+
+    Console.WriteLine("--- Sequência(s) Geradas ---\n");
+
+    for (int sequencia = 0; sequencia < quantidadeSequencias; sequencia++)
+    {
+        Console.Write($"Sequência {sequencia + 1:D2}: ");
+        for (int numero = 0; numero < QuantidadeNumerosPorSequencia; numero++)
+        {
+            Console.Write($"{listaSequencias[sequencia][numero]:D2}");
+            if (numero < QuantidadeNumerosPorSequencia - 1)
+                Console.Write(" - ");
+        }
+        Console.WriteLine();
+    }
+
+    Console.WriteLine();
+
+    Console.Write("Deseja gerar novas sequências? (S): ");
+    string? resposta = Console.ReadLine()!;
+
+    if (string.IsNullOrWhiteSpace(resposta) || resposta.Trim().ToUpper() != "S")
+    {
+        continuar = false;
+        Console.WriteLine("\nObrigado por usar o Surpresinha! Até mais!\n");
+    }
+    else
+    {
+        Console.Clear();
+        Console.WriteLine("Bem-vindo novamente ao Surpresinha!\n");
+    }
+}
+
--- README.md
+++ README.md
@@ -1,2 +1,83 @@
-# surpresinha
-p2 aula lógica 
+# Surpresinha
+
+Este software gera automaticamente números aleatórios de 1 a 99, organizados em "sequências" sem números repetidos.
+Ideal para gerar combinações para loterias ou para fins de sorteio.
+
+O usuário pode selecionar quantas sequências deseja gerar, e cada sequência terá 6 números sorteados e exibidos na tela.
+Ao final, o usuário pode optar por continuar sorteando ou encerrar o programa.
+
+
+## Exemplo
+
+```
+----------------------
+Bem-vindo ao Surpresinha!
+----------------------
+
+--- Geração de números Aleatórios ---
+
+Digite o número de sequências desejadas: 3
+
+
+--- Sequência(s) Geradas ---
+
+Sequência 01: 03 - 12 - 22 - 36 - 44 - 59
+Sequência 02: 05 - 15 - 28 - 31 - 47 - 60
+Sequência 03: 07 - 19 - 23 - 34 - 50 - 58
+
+
+Deseja gerar novas sequências? (S): s
+
+
+----------------------
+Bem-vindo novamente ao Surpresinha!
+----------------------
+
+--- Geração de números Aleatórios ---
+
+Digite o número de sequências desejadas: 1
+
+--- Sequência(s) Geradas ---
+
+Sequência 01: 06 - 17 - 25 - 32 - 41 - 53
+
+
+Deseja gerar novas sequências? (S): n
+
+Obrigado por usar o Surpresinha! Até mais!
+
+```
+
+## _Screenshots_
+
+Tela Inicial: 
+
+![Tela do Inicial](/Surpresinha/telas/inicial.png)
+
+![Tela de Geração](/Surpresinha/telas/geracao.png)
+
+![Tela Fim](/Surpresinha/telas/final.png)
+
+## _Download_
+
+Baixe o arquivo abaixo. Descompacte na paste desejada.
+
+[🔽 Download do arquivo .zip](dist/surpresinha.zip)
+
+Execulte utilizando o comando:
+
+```
+dotnet surpresinha.dll
+```
+
+Ou, se você estiver no Windows, pode dar um duplo-clique no ícone do programa.
+
+## Agradecimentos 
+
+- [Prof. Diego Neri](https://github.com/diegoneri)
+- [Dev C#](https://github.com/diegoneri/aulas-lp-csharp)
+- [Fatec Rubens Lara](https://fatecrl.edu.br/)
+
+---
+
+Todos os diretos reservados. PIRATARIA É CRIME.
